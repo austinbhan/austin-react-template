13e926a1602d7b6ef746ee3f3632e67d79bd795f
austinbhan <austinbhan@outlook.com>
2022-10-07 15:11:31 -0700

    dev for merge

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,11 @@
 import {
   BrowserRouter as Router,
-  Routes,
-  Route,
 } from 'react-router-dom';
-
-import Search from './components/Search/Search.jsx';
 
 export default function App() {
   return(
     <Router>
-      <Routes>
-        <Route path="search" element={<Search />} />
-      </Routes>
+      <h1>Hello World</h1>
     </Router>
   );
 }
